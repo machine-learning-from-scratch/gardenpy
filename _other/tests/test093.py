ddaadd8ade0eaa43bbb3d22cc94db4e1a34fe2c7
Christian Host-Madsen <143050709+chostmadsen@users.noreply.github.com>
2025-03-08 16:01:30 -1000

    Update test093.py
    switched from iteration to numpy broadcasting to speed up computation. This should be the finalized chain rule operation.

--- _other/tests/test093.py
+++ _other/tests/test093.py
@@ -22,32 +22,97 @@ def chain_opr(down: np.ndarray, up: np.ndarray) -> np.ndarray:
     return np.array(grad)
 
 
+def chain_opr_fast(down: np.ndarray, up: np.ndarray) -> np.ndarray:
+    initial_down_dims = down.shape
+    initial_up_dims = up.shape
+    # 6th dimensional downstream expansion
+    down = down[np.newaxis, np.newaxis, :, :, :, :]
+    down = np.tile(down, (initial_up_dims[0], initial_up_dims[1], 1, 1, 1, 1))
+    # 6th dimensional upstream expansion
+    up = up[:, :, :, :, np.newaxis, np.newaxis]
+    up = np.tile(up, (initial_down_dims[2], initial_down_dims[3]))
+    # 6th to 4th dimensional broadcast manipulation
+    return np.sum(down * up, axis=(2, 3))
+
+
+def chain_final(down: np.ndarray, up: np.ndarray) -> np.ndarray:
+    # 6th dimensional downstream expansion
+    down = down[np.newaxis, np.newaxis, :, :, :, :]
+    # 6th dimensional upstream expansion
+    up = up[:, :, :, :, np.newaxis, np.newaxis]
+    # 6th to 4th dimensional broadcast manipulation
+    return np.sum(down * up, axis=(2, 3))
+
+
+########################################################################################################################
+
+# matrix definition
 m1 = np.random.randn(5, 4)  # {5, 4}
 m2 = np.random.randn(4, 3)  # {4, 3}
 m3 = np.random.randn(1, 5)  # {2, 5}
 m4 = np.random.randn(3, 1)  # {3, 1}
 
+# matrix operations
 r1 = m1 @ m2  # {5, 3}
 r2 = m3 @ r1  # {2, 3}
 ls = r2 @ m4  # {2, 1}
 
-# local
+# local gradients
 # ls
-d_m4_ls = np.random.randn(2, 1, 3, 1)
-d_r2_ls = np.random.randn(2, 1, 2, 3)
+d_m4_ls = np.random.randn(2, 1, 3, 1)  # {2, 1, 3, 1}
+d_r2_ls = np.random.randn(2, 1, 2, 3)  # {2, 1, 2, 3}
 # r2
-d_m3_r2 = np.random.randn(2, 3, 2, 5)
-d_r1_r2 = np.random.randn(2, 3, 5, 3)
+d_m3_r2 = np.random.randn(2, 3, 2, 5)  # {2, 3, 2, 5}
+d_r1_r2 = np.random.randn(2, 3, 5, 3)  # {2, 3, 5, 3}
 # r1
-d_m2_r1 = np.random.randn(5, 3, 4, 3)
-d_m1_r1 = np.random.randn(5, 3, 5, 4)
+d_m2_r1 = np.random.randn(5, 3, 4, 3)  # {5, 3, 4, 3}
+d_m1_r1 = np.random.randn(5, 3, 5, 4)  # {5, 3, 5, 4}
 
-# 2-step
+# chained gradients
 # m1 to r2
-d_m1_r2 = np.random.randn(2, 3, 5, 4)
-d_m1_r2_calc = chain_opr(d_m1_r1, d_r1_r2)
-d_m2_r2_calc = chain_opr(d_m2_r1, d_r1_r2)
+d_m1_r2_calc = chain_opr(d_m1_r1, d_r1_r2)  # {2, 3, 5, 4}
+# m2 to r2
+d_m2_r2_calc = chain_opr(d_m2_r1, d_r1_r2)  # {2, 3, 4, 3}
 
-print(d_m1_r2.shape)
-print(d_m1_r2_calc.shape)
-print(d_m2_r2_calc.shape)
+# fast chained gradients
+# m1 to r2
+d_m1_r2_calc_fast = chain_opr_fast(d_m1_r1, d_r1_r2)  # {2, 3, 5, 4}
+# m2 to r2
+d_m2_r2_calc_fast = chain_opr_fast(d_m2_r1, d_r1_r2)  # {2, 3, 4, 3}
+
+# final chained gradients
+# m1 to r2
+d_m1_r2_calc_final = chain_final(d_m1_r1, d_r1_r2)  # {2, 3, 5, 4}
+# m2 to r2
+d_m2_r2_calc_final = chain_final(d_m2_r1, d_r1_r2)  # {2, 3, 4, 3}
+
+########################################################################################################################
+
+# shape testing
+print_shapes = True
+print_results_m1 = True
+
+if print_shapes:
+    print('-----------------------------------------------------------------------------------------------------------')
+    # slow
+    print(d_m1_r2_calc.shape)  # {2, 3, 5, 4}
+    print(d_m2_r2_calc.shape)  # {2, 3, 4, 3}
+    # fast
+    print(d_m1_r2_calc_fast.shape)  # {2, 3, 5, 4}
+    print(d_m2_r2_calc_fast.shape)  # {2, 3, 4, 3}
+    # final
+    print(d_m1_r2_calc_final.shape)  # {2, 3, 5, 4}
+    print(d_m2_r2_calc_final.shape)  # {2, 3, 4, 3}
+    print('-----------------------------------------------------------------------------------------------------------')
+
+if print_results_m1:
+    print('-----------------------------------------------------------------------------------------------------------')
+    # slow
+    print(d_m1_r2_calc)  # {2, 3, 5, 4}
+    print('-----------------------------------------------------------------------------------------------------------')
+    # fast
+    print(d_m1_r2_calc_fast)  # {2, 3, 5, 4}
+    print('-----------------------------------------------------------------------------------------------------------')
+    # final
+    print(d_m1_r2_calc_final)  # {2, 3, 5, 4}
+    print('-----------------------------------------------------------------------------------------------------------')
