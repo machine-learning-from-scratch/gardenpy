c7e3e802a2a2afba9dcdfd889bb1d3ff2f0b5299
chostmadsen25 <chostmadsen@gmail.com>
2025-03-12 11:47:24 -1000

    Update test100.py
    added relu and some more distinctions between some operations

--- _other/tests/test100.py
+++ _other/tests/test100.py
@@ -15,15 +15,21 @@ def four_broadcast_s(two_grad: np.ndarray) -> np.ndarray:
     return two_grad[np.newaxis, np.newaxis, :, :]
 
 
-def element_wise_operation(func: callable) -> callable:
+def elementwise_operation(func: callable) -> callable:
     def wrapper(main: np.ndarray, other: np.ndarray) -> np.ndarray:
-        return four_broadcast_e(func(main=main, other=other))
+        return four_broadcast_e(func(main, other))
+    return wrapper
+
+
+def lone_elementwise_operation(func: callable) -> callable:
+    def wrapper(main: np.ndarray) -> np.ndarray:
+        return four_broadcast_e(func(main))
     return wrapper
 
 
 def scalar_operation(func: callable) -> callable:
     def wrapper(main: np.ndarray, other: np.ndarray) -> np.ndarray:
-        return four_broadcast_s(func(main=main, other=other))
+        return four_broadcast_s(func(main, other))
     return wrapper
 
 
@@ -53,73 +59,82 @@ def d_matmul_o(main: np.ndarray, other: np.ndarray) -> np.ndarray:
     ...
 
 
-def ssr(main: np.ndarray, other: np.ndarray) -> np.ndarray:
-    return (np.sum(other - main) ** 2.0)[np.newaxis, np.newaxis]
+def lrelu(x: np.ndarray) -> np.ndarray:
+    return np.maximum(0.1 * x, x)
+
+
+@lone_elementwise_operation
+def d_lrelu(x: np.ndarray) -> np.ndarray:
+    return np.where(x > 0.0, 1.0, 0.1)
+
+
+def ssr(yhat: np.ndarray, y: np.ndarray) -> np.ndarray:
+    return (np.sum(y - yhat) ** 2.0)[np.newaxis, np.newaxis]
 
 
 @scalar_operation
-def d_ssr(main: np.ndarray, other: np.ndarray) -> np.ndarray:
-    return -2.0 * (other - main)
+def d_ssr(yhat: np.ndarray, y: np.ndarray) -> np.ndarray:
+    return -2.0 * (y - yhat)
 
 
-@element_wise_operation
+@elementwise_operation
 def d_pow(main: np.ndarray, other: np.ndarray) -> np.ndarray:
     two_grad = other * (main ** (other - 1.0))
     two_grad = np.where(np.isposinf(two_grad), 1e10, np.where(np.isneginf(two_grad), -1e10, two_grad))
     return two_grad
 
 
-@element_wise_operation
+@elementwise_operation
 def d_pow_o(main: np.ndarray, other: np.ndarray) -> np.ndarray:
     two_grad = np.log(main) * (main ** other)
     two_grad = np.where(np.isposinf(two_grad), 1e10, np.where(np.isneginf(two_grad), -1e10, two_grad))
     return two_grad
 
 
-@element_wise_operation
+@elementwise_operation
 def d_mul(main: np.ndarray, other: np.ndarray) -> np.ndarray:
     two_grad = other
     return two_grad
 
 
-@element_wise_operation
+@elementwise_operation
 def d_mul_o(main: np.ndarray, other: np.ndarray) -> np.ndarray:
     two_grad = main
     return two_grad
 
 
-@element_wise_operation
+@elementwise_operation
 def d_truediv(main: np.ndarray, other: np.ndarray) -> np.ndarray:
     two_grad = other ** -1.0
     return two_grad
 
 
-@element_wise_operation
+@elementwise_operation
 def d_truediv_o(main: np.ndarray, other: np.ndarray) -> np.ndarray:
     two_grad = -main / other ** 2.0
     two_grad = np.where(np.isposinf(two_grad), 1e10, np.where(np.isneginf(two_grad), -1e10, two_grad))
     return two_grad
 
 
-@element_wise_operation
+@elementwise_operation
 def d_add(main: np.ndarray, other: np.ndarray) -> np.ndarray:
     two_grad = np.ones(main.shape)
     return two_grad
 
 
-@element_wise_operation
+@elementwise_operation
 def d_add_o(main: np.ndarray, other: np.ndarray) -> np.ndarray:
     two_grad = np.ones(other.shape)
     return two_grad
 
 
-@element_wise_operation
+@elementwise_operation
 def d_sub(main: np.ndarray, other: np.ndarray) -> np.ndarray:
     two_grad = np.ones(main.shape)
     return two_grad
 
 
-@element_wise_operation
+@elementwise_operation
 def d_sub_o(main: np.ndarray, other: np.ndarray) -> np.ndarray:
     two_grad = -np.ones(other.shape)
     return two_grad
